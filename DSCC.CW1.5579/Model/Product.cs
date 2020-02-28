8a2b633f32115a2f7e7cb61dfdcf18df9707f102
Pardon <shahnozkarimova1997@gmail.com>
2020-02-28 18:56:56 +0500

    first commit

---
+++ DSCC.CW1.5579/Model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSCC.CW1._5579.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ DSCC.CW1.5579/Repository/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using DSCC.CW1._5579.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSCC.CW1._5579.Repository
+{
+    public interface IProductRepository
+    {
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int productId);
+        Product GetProductById(int id);
+        IEnumerable<Product> GetProducts();
+    }
+}
